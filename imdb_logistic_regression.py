63064bf67681f2091de8c1e02265a9383b372012
Mikhail Kravets <22kravets22@mail.ru>
2018-12-30 23:01:36 +0200

    Use tf-idf

--- imdb_logistic_regression.py
+++ imdb_logistic_regression.py
@@ -1,6 +1,9 @@
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
+from matplotlib import pyplot as pl
 
 from core import load_imdb2pandas
 
@@ -10,7 +13,7 @@ if __name__ == '__main__':
                                       db="imdb",
                                       collection="reviews")
 
-    vectorizer = CountVectorizer(stop_words="english",
+    vectorizer = TfidfVectorizer(stop_words="english",
                                  max_features=10000)
     training_features = vectorizer.fit_transform(training['text'])
 
@@ -19,4 +22,11 @@ if __name__ == '__main__':
 
     test_pred = model.predict(vectorizer.transform(test['text']))
     accuracy = accuracy_score(test_pred, test['sentiment'])
+
+    pca = TruncatedSVD(n_components=2)
+    tf2plot = pca.fit_transform(training_features)
+
+    # pl.plot(tf2plot[:, 0], tf2plot[:, 1], lw=0, ms=3, marker='.')
+    # pl.show()
+
     print(f"Accuracy: {accuracy}")
