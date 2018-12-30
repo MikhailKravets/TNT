0c058fbae8d624b2da32dc25570b318aa34147e5
Mikhail Kravets <22kravets22@mail.ru>
2018-12-30 20:24:31 +0200

    Write logistic regression predictor with the count vectorizer

--- imdb_logistic_regression.py
+++ imdb_logistic_regression.py
@@ -1,3 +1,6 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
 from core import load_imdb2pandas
 
 if __name__ == '__main__':
@@ -5,3 +8,11 @@ if __name__ == '__main__':
                           password="password",
                           db="imdb",
                           collection="reviews")
+
+    vectorizer = CountVectorizer(stop_words="english",
+                                 max_features=10000)
+    training_features = vectorizer.fit_transform(df['text'])
+
+    model = LogisticRegression()
+    model.fit(training_features, df['sentiment'])
+
