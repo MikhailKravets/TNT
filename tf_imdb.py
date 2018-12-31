708e54bffa1d802270dd3d4ce95291afec466501
Mikhail Kravets <22kravets22@mail.ru>
2018-12-31 17:36:42 +0200

    Prepare tf dataset from pandas training one

--- tf_imdb.py
+++ tf_imdb.py
@@ -7,3 +7,11 @@ if __name__ == '__main__':
                                       password="password",
                                       db="imdb",
                                       collection="reviews")
+    training_set = tf.data.Dataset.from_tensor_slices({'x': training['text'].values, 'y': training['sentiment'].values})
+    training_set = training_set.batch(32)
+
+    iterator = training_set.make_one_shot_iterator()
+
+    with tf.Session() as sess:
+        val = sess.run(iterator.get_next())
+
