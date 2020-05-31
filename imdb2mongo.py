a430e3ed9e69ee1eb13741096557593bb8489252
mikhailkravets <michkravets@gmail.com>
2020-05-31 19:23:04 +0300

    Remove redundant code

--- imdb2mongo.py
+++
@@ -1,8 +0,0 @@
-from core import load_imdb2mongo
-
-
-if __name__ == '__main__':
-    load_imdb2mongo(".data/aclImdb",
-                    mongo_user="root",
-                    mongo_password="password",
-                    mongo_db="imdb")
