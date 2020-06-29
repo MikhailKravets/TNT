74468c61746a31e426f986e6b8d38c6c5d76f757
mikhailkravets <michkravets@gmail.com>
2020-06-29 11:32:03 +0300

    Found error in training

--- imbd_classification.py
+++ imbd_classification.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
@@ -8,11 +9,14 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
+log_dir = ".data/logs/fit/"
+
+
 def preprocess_text(sen):
     sen = re.sub(r'[^a-zA-Z]', ' ', sen)
     sen = re.sub(r'\s+', ' ', sen)
     sen = re.sub(r'\s+[a-zA-Z]\s+', ' ', sen)
-    return sen
+    return sen.lower()
 
 
 if __name__ == '__main__':
@@ -28,27 +32,30 @@ if __name__ == '__main__':
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(data['review'])
 
-    inp = pad_sequences(tokenizer.texts_to_sequences(data['review']), maxlen=1600)
+    # TODO: split on train, test data
+    inp = pad_sequences(tokenizer.texts_to_sequences(data['review']), maxlen=1000)
     dataset = tf.data.Dataset.from_tensor_slices((inp, np.expand_dims(data['sentiment'].values, 1)))
     test_dataset = dataset.take(10_000)
     dataset = dataset.skip(10_000)
 
     model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(99426, 64),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.Conv1D(64, 3, 3, padding='same', activation='relu'),
-        tf.keras.layers.MaxPool1D(pool_size=2, padding='same'),
-        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Embedding(99426, 40, input_length=1000),
+        tf.keras.layers.Dropout(0.1),
         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
         tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(10),
-        tf.keras.layers.Softmax()
+        tf.keras.layers.Dense(1, activation='softmax'),
     ])
 
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(0.0001),
-        loss=tf.losses.BinaryCrossentropy(),
+        optimizer=tf.keras.optimizers.Adam(1e-3),
+        loss=tf.losses.BinaryCrossentropy(from_logits=True),
         metrics=['accuracy']
     )
     print(model.summary())
-    model.fit(dataset, batch_size=32, validation_data=test_dataset, validation_steps=30, epochs=4)
+
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, update_freq='batch')
+    model.fit(inp, np.expand_dims(data['sentiment'].values, 1), batch_size=32,
+              validation_data=test_dataset,
+              validation_steps=30, epochs=4,
+              shuffle=True,
+              callbacks=[tensorboard_callback])
