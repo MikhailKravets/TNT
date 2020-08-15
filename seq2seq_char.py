a04f1047bc8497c1240eca7b78b88ecf7804cb55
mikhailkravets <michkravets@gmail.com>
2020-08-15 18:13:41 +0300

    Finish char level text generation endpoint

--- seq2seq_char.py
+++ seq2seq_char.py
@@ -1,10 +1,11 @@
+"""The example taken from https://github.com/llSourcell/AI_In_Marketing/blob/master/text_generation.py"""
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.python.keras import Model
 
 batch_size = 64  # Batch size for training.
-epochs = 100  # Number of epochs to train for.
+epochs = 30  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
@@ -95,3 +96,59 @@ if __name__ == '__main__':
               batch_size=batch_size,
               epochs=epochs,
               validation_split=0.2)
+
+    encoder_model = Model(encoder_inp, [state_h, state_c])
+
+    decoder_state_input_h = layers.Input(shape=(latent_dim,))
+    decoder_state_input_c = layers.Input(shape=(latent_dim,))
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    decoder_outputs, state_h, state_c = decoder(decoder_inp,
+                                                initial_state=[decoder_state_input_h, decoder_state_input_c])
+    decoder_outputs = dense(decoder_outputs)
+    decoder_model = Model(
+        [decoder_inp] + decoder_states_inputs,
+        [decoder_outputs] + [state_h, state_c])
+
+    # Reverse-lookup token index to decode sequences back to
+    # something readable.
+    reverse_input_char_index = dict(
+        (i, char) for char, i in input_token_index.items())
+    reverse_target_char_index = dict(
+        (i, char) for char, i in target_token_index.items())
+
+    def decode_sequence(input_seq):
+        # Encode the input as state vectors.
+        states_value = encoder_model.predict(input_seq)
+
+        # Generate empty target sequence of length 1.
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        # Populate the first character of target sequence with the start character.
+        target_seq[0, 0, target_token_index['\t']] = 1.
+
+        # Sampling loop for a batch of sequences
+        # (to simplify, here we assume a batch of size 1).
+        stop_condition = False
+        decoded_sentence = ''
+        while not stop_condition:
+            output_tokens, h, c = decoder_model.predict(
+                [target_seq] + states_value)
+
+            # Sample a token
+            sampled_token_index = np.argmax(output_tokens[0, -1, :])
+            sampled_char = reverse_target_char_index[sampled_token_index]
+            decoded_sentence += sampled_char
+
+            # Exit condition: either hit max length
+            # or find stop character.
+            if (sampled_char == '\n' or
+                    len(decoded_sentence) > max_decoder_seq_length):
+                stop_condition = True
+
+            # Update the target sequence (of length 1).
+            target_seq = np.zeros((1, 1, num_decoder_tokens))
+            target_seq[0, 0, sampled_token_index] = 1.
+
+            # Update states
+            states_value = [h, c]
+
+        return decoded_sentence
