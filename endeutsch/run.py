a253a842f50d944416ecc0c3323c7d5aaf66e718
mikhailkravets <michkravets@gmail.com>
2020-11-08 19:55:54 +0200

    Add new package

---
+++ endeutsch/run.py
@@ -0,0 +1,7 @@
+# TODO: prepare dataset: download, clear, tokenize
+# TODO: create Encoder model
+# TODO: create Decoder model
+# TODO: create TF dataset
+# TODO: create train function, create loss function
+# TODO: train model
+# TODO: evaluate results
